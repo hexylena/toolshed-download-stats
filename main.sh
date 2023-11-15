5e3a79e128d41b828617f87abeac79c55fc7e23b
Helena Rasche <hexylena@galaxians.org>
2023-11-15 17:23:49 +0100

    cleanups

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-curl https://toolshed.g2.bx.psu.edu/api/repositories > repos.json
+# curl https://toolshed.g2.bx.psu.edu/api/repositories > repos.json
 cat repos.json| jq '[.[] | {"key": (.owner +"/" + .name), "value": (.times_downloaded) }] | from_entries' -S > downloads.json
 cat repos.json| jq '.[] | [.owner +"/" + .name, .times_downloaded] | @tsv' -r > downloads.tsv
 
@@ -8,10 +8,25 @@ cat repos.json | jq '[.[] | {"owner": .owner, "dls": .times_downloaded}] | group
 cat repos.json | jq '[.[] | {"owner": .owner, "dls": .times_downloaded}] | group_by(.owner) | map([first.owner, (map(.dls) | add)]) | .[] | @tsv' -r > downloads-by-user.tsv
 
 cat head.html > downloads-by-user.html
-echo "<h1>Downloads by User</h1>" >> downloads-by-user.html
-echo "<p>Collected from the Toolshed API, also available as downloads-by-user.json</p>" >> downloads-by-user.html
-echo "<table><thead><tr><th scope='col'>Owner</th><th scope='col'>Downloads</th></tr></thead><tbody>" >> downloads-by-user.html
+cat >> downloads-by-user.html <<-EOF
+	<header class="container">
+		<h1>Downloads by User</h1>
+		<nav aria-label="breadcrumb">
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li>Downloads by User (HTML)</li>
+			</ul>
+		</nav>
+	</header>
+	<main class="container">
+	<p>Collected from the Toolshed API, updated weekly.</p>
+	<table>
+		<thead>
+			<tr><th scope='col'>Owner</th><th scope='col'>Downloads</th></tr>
+		</thead>
+		<tbody>
+EOF
+
 cat downloads-by-user.json | jq '. | to_entries[] | "<tr><td>" + .key + "</td><td>" + (.value | tostring) + "</td></tr>"' -r  >> downloads-by-user.html
 echo "</tbody></table>" >> downloads-by-user.html
 cat tail.html >> downloads-by-user.html
-
